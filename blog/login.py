cfd995bc0797d8890293ba4ebc40f8b08e3ce073
niewanroot <opa86@163.com>
2018-05-31 16:54:29 +0800

    添加了一些功能登录

---
+++ blog/login.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import include,url
+from django.contrib import admin
+
+from django.contrib.auth import views
+
+urlpatterns=[
+    url(r'^admin/',admin.site.urls),
+    url(r'',include('blog.urls')),
+    ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from django.contrib.auth import views
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
@@ -8,4 +10,5 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'',include('blog.urls')),
+    url(r'^accounts/login/$',views.login, name='login'),
 ]
