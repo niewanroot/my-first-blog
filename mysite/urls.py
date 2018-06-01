4c17187fa619915f7cf1c2bbc21c0929fea1862b
niewanroot <opa86@163.com>
2018-06-01 15:16:30 +0800

    优化些东西添加评论非常粗糙的博客

--- mysite/urls.py
+++ mysite/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'',include('blog.urls')),
     url(r'^accounts/login/$',views.login, name='login'),
+    url(r'^accounts/logout/$',views.logout,name='logout',kwargs={'next_page':'/'}),
 ]
